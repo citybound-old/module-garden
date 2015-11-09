4ba2aaa4ef026ebea8099a8c78d06c012478d37a
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2015-11-09 21:59:39 +0100

    cleanup and actually delete idea stuff

--- index.es6.js
+++ index.es6.js
@@ -1,44 +1,65 @@
-import WalledGarden from "walled-garden";
+import metaEval from "meta-eval";
 import fs from "fs";
+import {transform} from "babel-core";
+
+const PrintTranspilation = true;
 
 export default class ModuleGarden {
-	constructor ({name: gardenName, modules: filePatterns, environment: intialEnvironment, nativeExtensions=[], transpile=false}) {
-		this.walledGarden = new WalledGarden(intialEnvironment);
+	constructor ({
+		name: gardenName,
+		modules: filePatterns,
+		environment: initialEnvironment,
+		dependencies,
+		afterDependencies,
+		dependencyResolver,
+		nativeExtensions=[], transpile=false, strict=true
+	}) {
+		let sourceUrlBase = "moduleGarden://" + gardenName;
+		this.modules = {};
+
+		for (let dependencyIdentifier of Object.keys(dependencies)) {
+			global[dependencyIdentifier] = module.parent.require(dependencies[dependencyIdentifier]);
+			this.modules[dependencyIdentifier] = new Module(dependencyIdentifier, {isDependency: true});
+		}
+
+		if (afterDependencies) afterDependencies();
+
+		global.require = (name) => {
+			if (global[name])
+				if (this.modules[name].loaded) return global[name];
+				else return this.modules[name].load(this.walledGarden);
+			else return module.parent.require(name);//throw `Error: cannot find module ${name} in ModuleGarden ${gardenName}`;
+		};
+
+		let filenames = [];
+		for (let pattern of filePatterns)
+			explore(".", pattern.split("/"), filenames);
+
+		for (let filename of filenames) {
+			let basename = getBasename(filename);
+			let name = basename.substr(0, basename.lastIndexOf("."));
+
+			global[name] = {};
+			this.modules[name] = new Module(filename, {transpile, strict});
+		}
 
 		for (let filename of nativeExtensions) {
 			let name = getBasename(filename);
 
-			let source = getSource(filename, true, transpile);
-			source = source + "\n" + `//# sourceURL=moduleGarden://${name}${filename}`;
+			let source = getSource("./" + filename, true, transpile);
+			source = source + "\n" + `//# sourceURL=mg://${gardenName}/${filename.replace("./", "")}`;
 
-			this.walledGarden.run(source);
+			metaEval(source, {}, name, filename, sourceUrlBase);
 		}
 
-		let filenames = [];
-		explore(".", pattern.split("/"), filenames);
-
-		for (let filename of filenames) {
-			let name = getBasename(filename);
-
-			if (typeof this.walledGarden.environment[name] === "undefined")
-				this.walledGarden.environment[name] = {};
-
-			let source = getSource(filename, true, transpile);
-			source =
-`var exports = ${name}, module = {exports: exports};
-
-${source}
-
-if (module.exports !== exports) global.${name} = module.exports}
-//# sourceURL=moduleGarden://${name}${filename}`;
-
-			this.walledGarden.run(source);
+		for (let moduleName of Object.keys(this.modules)) {
+			this.modules[moduleName].load(sourceUrlBase);
 		}
-
-
 	}
 
-
+	get environment () {
+		return global
+	}
 }
 
 function explore(path, parts, filenames) {
@@ -64,7 +85,7 @@ function getBasename(filename) {
 	return filename.substr(filename.lastIndexOf("/") + 1);
 }
 
-function getSource(filename, strict=true, transpile=false) {
+function getSource(filename, transpile=false) {
 	function stripBOM(content) {
 		// Remove byte order marker. This catches EF BB BF (the UTF-8 BOM)
 		// because the buffer-to-string conversion in `fs.readFileSync()`
@@ -78,10 +99,7 @@ function getSource(filename, strict=true, transpile=false) {
 	var sourceStat = fs.statSync(filename);
 	var source = stripBOM(fs.readFileSync(filename, 'utf8'));
 
-	if (strict)
-		source = '"use strict";\n' + source;
-
-	if (transpile &&  filename.indexOf("./engine") !== -1 || filename.indexOf("./game") !== -1) {
+	if (filename.indexOf("./engine") !== -1 || filename.indexOf("./game") !== -1) {
 		var cacheStat;
 		var cacheFilename = ".babelCache/" + filename.slice(2);
 		try {
@@ -93,13 +111,12 @@ function getSource(filename, strict=true, transpile=false) {
 		var transpiled;
 
 		if (!cacheStat || cacheStat.mtime < sourceStat.mtime) {
-			transpiled = babel.transform(source, {
-				blacklist: ["regenerator", "es6.tailCall"],
+			transpiled = transform(source, {
+				blacklist: ["regenerator", "es6.tailCall"/*, "es6.forOf"*/],
 				loose: ["es6.forOf"],
 				optional: ["es7.classProperties"],
 				filename: filename,
-				sourceMaps: false,//"inline",
-				sourceMapName: sourceUrlBase + filename.replace("./", "")
+				sourceMaps: false,
 			});
 
 			cacheFilename.split('/').reduce(function(prev, curr, i) {
@@ -126,4 +143,37 @@ function getSource(filename, strict=true, transpile=false) {
 	}
 
 	return source;
+}
+
+class Module {
+	constructor (filename, {transpile=false, strict=true, isDependency=false}) {
+		let basename = getBasename(filename)
+		this.filename = filename;
+		this.name = basename.substr(0, basename.lastIndexOf("."));
+		this.loaded = isDependency || false;
+		this.source = !isDependency && getSource(filename, transpile);
+		this.exports = {};
+		this.strict = strict;
+	}
+
+	load (sourceUrlBase) {
+		if (this.loaded) return global[this.name];
+
+		let source = this.source;
+
+		source =
+`var exports = ${this.name}, module = {exports: exports};
+
+(function() {${source}})();
+
+if (module.exports !== exports) global.${this.name} = module.exports;`;
+
+		if (this.strict) source = '"use strict";\n' + source;
+
+		metaEval(source, {}, this.name, this.filename, sourceUrlBase);
+
+		this.loaded = true;
+
+		return global[this.name];
+	}
 }
